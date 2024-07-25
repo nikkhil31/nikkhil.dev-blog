265e73ea8d7508bbc627bede5911f6f7dde01258
nikkhil31 <nikhilimbad@gmail.com>
2024-07-25 09:34:46 +0530

    fixed blog routing

--- src/core/constant/env.ts
+++ src/core/constant/env.ts
@@ -1,4 +1,5 @@
 export const siteUrl = 'nikkhil.dev/blog'
+export const host = 'nikkhil.dev'
 export const social = {
 	linkedin: 'https://www.linkedin.com/in/nikhil-limbad-a511b5182/',
 	mail: 'mailto:me@nikkhil.dev',
