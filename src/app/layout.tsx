5f316cb15450d82734ab9585843701e185ebd288
nikkhil31 <nikhilimbad@gmail.com>
2024-07-25 23:34:43 +0530

    Fixed meta title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
 	title:
 		'Nikhil Limbad - Freelance Full Stack Developer in India | Expert in NextJS, React, AWS, Langchain',
 	description:
-		'Nikhil Limbad is an experienced Freelance Full Stack Developer in India, specializing in NextJS, React, AWS, and OpenAI integration. Over 4 years of experience in diverse sectors. Open for remote freelance opportunities.',
+		'Nikhil Limbad is an experienced Freelance Full Stack Developer in India, specializing in NextJS, React, AWS, and OpenAI integration. Over 5 years of experience in diverse sectors. Open for remote freelance opportunities.',
 	keywords:
 		'Nikhil Limbad, Freelance Full Stack Developer, Freelance Full Stack Developer India, Expert NextJS Developer, Experienced Full Stack Developer, React, AWS, OpenAI'
 }
